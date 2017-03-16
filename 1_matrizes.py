22ce46a5e97e2e15e21d0d7b4b0ee0232a427ee2
Matheus A. Souza <ticksmas@gmail.com>
2017-03-16 08:18:32 -0300

    Iniciando repositório

---
+++ 1_matrizes.py
@@ -0,0 +1,27 @@
+#!/bin/python
+# -*- coding: utf-8 -*-
+
+# A biblioteca 'numpy' contém um excelente pacote para trabalhar com matrizes
+import numpy as np
+
+# Definindo uma matriz 5x5 qualquer
+matriz = np.array([ 
+                [ 1, 3, 4, 6, 7] ,
+                [ 3, 1,-2, 7, 9] ,
+                [-4, 4, 6, 3, 6] ,
+                [ 9, 0, 6,-6, 8] ,
+                [ 2, 1,-2, 5, 0] ])
+
+print matriz.dtype
+# Soma algébrica
+# Multiplicação
+# Triangular inferior e superior
+# É diagonal / É identidade / É simétrica 
+# Inversa
+# Transposta
+# Adjunta
+# Determinante
+# Menor complementar
+# Posto
+# Autovalor
+# Autovetor
