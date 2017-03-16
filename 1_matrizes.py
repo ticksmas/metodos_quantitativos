d468f875448e02e32315c82307d445a4aa562e8c
Matheus A. Souza <ticksmas@gmail.com>
2017-03-16 08:35:47 -0300

    Iniciando matrizes. Usando operação de soma.

--- 1_matrizes.py
+++ 1_matrizes.py
@@ -1,19 +1,27 @@
-#!/bin/python
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
 
 # A biblioteca 'numpy' contém um excelente pacote para trabalhar com matrizes
 import numpy as np
 
-# Definindo uma matriz 5x5 qualquer
-matriz = np.array([ 
-                [ 1, 3, 4, 6, 7] ,
-                [ 3, 1,-2, 7, 9] ,
-                [-4, 4, 6, 3, 6] ,
-                [ 9, 0, 6,-6, 8] ,
-                [ 2, 1,-2, 5, 0] ])
+# Definindo matrizes 5x5
+m1 = np.random.randint(-9, 10, size=(5, 5))
+m2 = np.random.randint(-9, 10, size=(5, 5))
+print 'Matriz 1:'
+print m1
 
-print matriz.dtype
+print '\nMatriz 2:' 
+print m2
+
 # Soma algébrica
+soma = np.add(m1, m2)
+print '\nSoma M1 + M2:'
+print soma
+
+soma = np.add(4, m1)
+print '\nSoma 4 + M1:'
+print soma
+
 # Multiplicação
 # Triangular inferior e superior
 # É diagonal / É identidade / É simétrica 
