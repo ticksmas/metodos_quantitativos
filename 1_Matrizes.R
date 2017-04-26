bf0722bf8a364d798b1ac3fcca2abc6eb4996f7f
MarcosWRodrigues <marcoswanderrodrigues@gmail.com>
2017-04-26 10:51:59 -0300

    Update 1_Matrizes.R

--- 1_Matrizes.R
+++ 1_Matrizes.R
@@ -4,11 +4,13 @@
 # Alunos    : Matheus Alcantara
 #             Pedro Ruas
 #             Marcos W. Rodrigues
+# Abordagens: Matrizes (Definições, Operações, Produto, Matrizes triangulares,
+#                      Identidade, Inversa, Transposta, Simétrica, Menor complemen-
+#                      tário, Cofator, Matriz adjunta, Determinante, Posto (Rank),
+#                      Auto-Valores e Auto-Vetores, Operador linear
 #--------------------------------------------------------------------------------
 
-#---------- Aula 14/03/2017
-
-# Definindo matrizes 3x3
+# Definindo matrizes
 
 A <- matrix(sample(0:9,9), nrow=3, ncol=3)
 B <- matrix(sample(0:9,9), nrow=3, ncol=3)
@@ -19,13 +21,17 @@ B <- matrix(sample(0:9,9), nrow=3, ncol=3)
 cat("Matriz A:\n") ; A
 cat("Matriz B:\n") ; B
 
+#----------
+
 # Operacoes com matrizes
 
 cat("Soma dos elementos das matrizes (A+B):\n") ; A+B
 cat("Escalar multiplica cada elemento da matriz (3*A):\n") ; A ; 3*A
 cat("Multiplicacao dos elementos das matrizes (A*B):\n") ; A ; B ; A*B
 cat("Multiplicacao de matrizes (A*B):\n") ; A ; B ; A %*% B
-cat("Multiplicacao da matriz pela matriz identidade resulta na propria matriz (A*I):\n") ; A ; diag(3) ; A %*% diag(3)
+I <- diag(1,nrow=dim(A)[1]) #Matriz identidade
+cat("Matriz identidade de tamanho 3:\n") ; I
+cat("Multiplicacao da matriz pela matriz identidade resulta na propria matriz (A*I):\n") ; A ; diag(3) ; A %*% I
 
 cat("Diagonal da matriz da matriz A:\n") ; diag(A)
 
@@ -38,12 +44,25 @@ cat("Multiplicacao da matriz inversa de A pela matriz B (A'*B):\n") ; crossprod(
 cat("Determinante da matriz A - (det(A)):\n") ; det(A)
 cat("Determinante da matriz transposta A - det(T(A)):\n") ; det(t(A))
 
-cat("Decomposicao simples da matriz A:\n") ; y <- svd(A) ; y # y$d ; y$u ; y$v
-#cat("Decomposicao simples - Valores singulares:\n") ; y$d
-#cat("Decomposicao simples - Vetores singular a esquerda de A:\n") ; y$u
-#cat("Decomposicao simples - Vetores singular a direita de A:\n") ; y$v
+#----------
 
-#Cofator
+# Posto ou Rank
+
+cat("Posto (rank) da matriz A:\n") ; A ; matrix(rank(A), nrow=length(A[,1]), ncol=length(A[1,]))
+
+#----------
+
+# Triangular superior e inferior
+
+library(base)
+C <- matrix(1:20, 4, 5)
+cat("Triangular superior:\n") ; upper.tri(C, diag=TRUE)
+cat("Triangular inferior:\n") ; lower.tri(C, diag=TRUE)
+
+#----------
+
+# Cofator
+
 A <- matrix(data=c(1, 2, 3, 2, 5, 9, 5, 7, 8), nrow=3, ncol=3)
 minor <- function(A, i, j) det( A[-i,-j] )
 cofactor <- function(A, i, j) (-1)^(i+j) * minor(A,i,j)
@@ -69,9 +88,9 @@ cat("A matriz A eh simetrica da matriz B?:\n") ; A == B
 cat("A matriz A eh simetrica da matriz B?:\n") ; A == t(A)
 isSymmetric.matrix(B)
 
-#---------- Aula 21/03/2017
+#----------
 
-# Sistemas de equacoes
+# Sistemas de equacoes lineares
 
 #  x + 2y + 3z = 20  
 # 2x + 5y + 9z = 100  
@@ -85,6 +104,8 @@ cat("Matriz A:\n") ; A
 cat("Matriz x:\n") ; x
 cat("Matriz solucao b:\n") ; b
 
+#----------
+
 # Autovalores e autovetores
 
 #A <- matrix(sample(0:9,9), nrow=3, ncol=3)
@@ -92,6 +113,8 @@ A <- matrix(c(3,-1,1,-1,5,-1,1,-1,3), nrow=3, ncol=3)
 y <- eigen(A)
 cat("Autovalores da matriz A:\n") ; y$values
 cat("Autovetores da matriz A:\n") ; round(y$vectors)
+
+#----------
 
 # Metodo de decomposicao/fatorizacao dos minimos quadrados
 
@@ -101,6 +124,8 @@ B <- matrix(c(3,2,0,2,3,0,0,0), nrow=4, ncol=2)
 mq <- lsfit(A,B, wt=NULL, intercept=TRUE, tolerance=1.e-07,yname=NULL)
 cat("Metodo dos minimos quadrados:\n") ; ls.print(mq) #mq
 #s <- solve(t(A) %*% B) %*% t(A) %*% A[,1]
+
+#----------
 
 # Metodo de reducao Gauss-Jordan
 
@@ -112,6 +137,34 @@ cat("Matriz A:\n") ; A
 
 cat("Linha reduzida:\n") ; rref(A)
 
-#--------------------------------------------------------------------------------
-#--------------------------------------------------------------------------------
+#----------
 
+#Matriz 6x4
+Xij <- matrix(seq(1:24), ncol = 4) 
+rownames(Xij) <- paste("S", seq(1, dim(Xij)[1]), sep="")
+colnames(Xij) <- paste("V", seq(1, dim(Xij)[2]), sep="")
+Xij
+
+#Vetor de 1s
+um <- rep(1, dim(Xij)[1])
+
+#Media da coluna 
+X.means <- t(um) %*% Xij / dim(Xij)[1] #CÃ¡lculo por etapas
+colMeans(Xij) #CÃ¡lculo direto
+
+X.diff <- Xij - um %*% X.means
+
+X.cen <- scale(Xij,scale=FALSE) #MÃ©dia do centro da matriz
+
+#----------
+
+#Variancia e Covariancia
+
+X.cov <- t(X.diff) %*% X.diff / (dim(X.diff)[1]-1)
+round(X.cov)
+
+round(cov(Xij),2) #CovariÃ¢ncia
+cor(Xij) #CorrelaÃ§Ã£o
+
+#----------
+#----------
